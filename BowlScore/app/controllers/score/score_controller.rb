e07cd2740f1a8f0cc9c48387e354ff8a2eb4443d
anthonyvia <anthv123@gmail.com>
2014-06-07 14:29:18 -0700

    Clean code.

--- BowlScore/app/controllers/score/score_controller.rb
+++ BowlScore/app/controllers/score/score_controller.rb
@@ -1,12 +1,5 @@
 class Score::ScoreController < ApplicationController
 
-  def hello
-
-    text = "hey sup"
-
-    render :status => :ok, :json => text.to_json
-  end
-
   def calculate_score
 
     game = Score::Game.new(params[:game].to_json)
